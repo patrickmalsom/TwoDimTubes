2e170478936600c7fbc99901bec84c020b4f8303
Patrick Malsom <patrickmalsom@gmail.com>
2013-05-14 12:21:51 -0400

    removed the full file path from the mathematica script for averaging the files. the script now requires you are in the correct directory to be able to do the averaging.

--- meanconf.m
+++ meanconf.m
@@ -2,14 +2,14 @@
 
 argv=$ScriptCommandLine;
 
-importDir = argv[[2]];
-Num = ToExpression[argv[[3]]];
+Num = ToExpression[argv[[2]]];
 
-tempData = Import[importDir <> "2WellTubes-T0.15-pos0010000.dat", "Table"];
+tempData=Import["2WellTubes-T0.15-pos0010000.dat", "Table"];
+
 Do[
-  tempData = tempData + Import[importDir<>"2WellTubes-T0.15-pos"<>StringDrop[ToString[NumberForm[i, 3, NumberPadding -> {"0", ""}]], 1] <> "0000.dat", "Table"];
+  tempData = tempData + Import["2WellTubes-T0.15-pos"<>StringDrop[ToString[NumberForm[i, 3, NumberPadding -> {"0", ""}]], 1] <> "0000.dat", "Table"];
 ,{i, 2, Num}];
 
-Export[importDir<>"meanpath.dat",tempData/Num,"TSV"];
+Export["meanpath.dat",tempData/Num,"TSV"];
 
 Quit[];
